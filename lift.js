ac43ca613cf8f14fface9e1ad5b727211709f3df
Hunter Funk <hunter.d.funk@gmail.com>
2021-05-17 15:29:14 -0700

    finished styling, working on animations

---
+++ lift.js
@@ -0,0 +1,7 @@
+// var Uix = document.getElementById(`Uix`);
+
+// let lift = () => {
+//     document.getElementById(`Uix`).style.animation = `lift 3s linear 1 forward`;
+// }
+
+// Uix.addEventListener(`mouseenter`, lift);
---
+++ resHead.js
@@ -0,0 +1,7 @@
+var string = "My name is Hunter Funk, and I am a...";
+var str = string.split("");
+var el = document.getElementById('resHead');
+(function animate() {
+str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
+var running = setTimeout(animate, 90);
+})();
